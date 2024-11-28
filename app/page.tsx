97ff8155b35ce2f5d68382b777146e82975f47d0
satoshi <uglysatoshi@ya.ru>
2024-11-28 05:11:14 +0300

    fixed homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import '@/styles/globals.css';
@@ -15,15 +15,23 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useRouter } from "next/navigation";
 import { useUserContext } from "@/context/UserContext";
-import Link from "next/link";
-
 
 export default function Home() {
-
     const { setUserData } = useUserContext();
     const router = useRouter();
 
-    const [formData, setFormData] = useState({ name: "", group: "", date: "", score: 0, theme1: 0, theme2: 0, theme3: 0, theme4: 0, theme5: 0, theme6: 0});
+    const [formData, setFormData] = useState({
+        name: "",
+        group: "",
+        date: "",
+        score: 0,
+        theme1: 0,
+        theme2: 0,
+        theme3: 0,
+        theme4: 0,
+        theme5: 0,
+        theme6: 0,
+    });
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { id, value } = e.target;
@@ -31,10 +39,14 @@ export default function Home() {
     };
 
     const handleSubmit = () => {
-        setUserData(formData); // Сохраняем данные в контекст
-        router.push("/quiz"); // Переходим на страницу теста
+        if (formData.name && formData.group && formData.date) {
+            setUserData(formData); // Сохраняем данные в контекст
+            router.push("/quiz"); // Переходим на страницу теста
+        }
     };
 
+    // Проверяем, заполнены ли все поля
+    const isFormComplete = formData.name && formData.group && formData.date;
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen p-4">
@@ -48,27 +60,43 @@ export default function Home() {
                         <div className="grid w-full items-center gap-4">
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="name">Имя</Label>
-                                <Input id="name" placeholder="Ложкин Иван Андреевич" onChange={handleChange}/>
+                                <Input
+                                    id="name"
+                                    placeholder="Ложкин Иван Андреевич"
+                                    onChange={handleChange}
+                                />
                             </div>
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="group">Группа</Label>
-                                <Input id="group" placeholder="4-ТИД-7" onChange={handleChange}/>
+                                <Input
+                                    id="group"
+                                    placeholder="4-ТИД-7"
+                                    onChange={handleChange}
+                                />
                             </div>
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="framework">Дата прохождения</Label>
-                                <Input type="date" id="date" onChange={handleChange}/>
+                                <Input
+                                    type="date"
+                                    id="date"
+                                    onChange={handleChange}
+                                />
                             </div>
                         </div>
                     </form>
                 </CardContent>
                 <CardFooter className="flex justify-center items-center">
-                    <Button onClick={handleSubmit}>
-                        <Link href="/quiz" legacyBehavior passHref>
-                            Начать
-                        </Link>
+                    <Button
+                        onClick={handleSubmit}
+                        disabled={!isFormComplete} // Кнопка неактивна, если форма не заполнена
+                        className={`w-full ${
+                            !isFormComplete ? "opacity-50 cursor-not-allowed" : ""
+                        }`}
+                    >
+                        Начать
                     </Button>
                 </CardFooter>
             </Card>
         </div>
-    )
+    );
 }
